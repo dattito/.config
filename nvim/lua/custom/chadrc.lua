cebf4fabc4daf5942fd26f97ae21cbe8bd83d631
dattito <32016658+dattito@users.noreply.github.com>
2023-07-29 08:58:46 +0200

    feat: edit chadrc.lua

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -1,6 +1,20 @@
 ---@type ChadrcConfig
 local M = {}
-M.ui = { theme = "catppuccin" }
+
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
+
+M.ui = {
+  theme = "catppuccin",
+  transparency = true,
+
+  nvdash = {
+    load_on_startup = true,
+  },
+
+  statusline = {
+    theme = "minimal",
+  },
+}
+
 return M
