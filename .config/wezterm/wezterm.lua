c6cb32b89821519a7b072c380017972c51fc4966
dattito <32016658+dattito@users.noreply.github.com>
2024-06-10 14:26:32 +0200

    feat(zsh): remove fzf background

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,28 +1,28 @@
 local wezterm = require("wezterm")
 
 return {
-	tab_bar_at_bottom = true,
-	hide_tab_bar_if_only_one_tab = true,
-	window_decorations = "RESIZE",
+  tab_bar_at_bottom = true,
+  hide_tab_bar_if_only_one_tab = true,
+  window_decorations = "RESIZE",
 
-	window_background_opacity = 0.93,
-	macos_window_background_blur = 32,
+  window_background_opacity = 0.93,
+  macos_window_background_blur = 32,
 
-	font = wezterm.font("FiraCode Nerd Font"),
-	font_size = 14.0,
+  font = wezterm.font("FiraCode Nerd Font"),
+  font_size = 14.0,
 
-	color_scheme = "catppuccin-mocha",
+  color_scheme = "catppuccin-mocha",
 
-	use_dead_keys = false,
-	send_composed_key_when_left_alt_is_pressed = true,
-	use_ime = true,
+  use_dead_keys = false,
+  send_composed_key_when_left_alt_is_pressed = true,
+  use_ime = true,
 
-	window_close_confirmation = "NeverPrompt",
-	keys = {
-		{
-			key = "n",
-			mods = "OPT",
-			action = wezterm.action({ SendString = "~" }),
-		},
-	},
+  window_close_confirmation = "NeverPrompt",
+  keys = {
+    {
+      key = "n",
+      mods = "OPT",
+      action = wezterm.action({ SendString = "~" }),
+    },
+  },
 }
