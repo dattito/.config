cbf0455bd4d56448f3c6d716cb8521f7dd37501a
dattito <david.siregar@sap.com>
2024-06-10 09:53:41 +0200

    fea(nvim): add precognition.nvim

---
+++ .config/nvim/lua/essi_datti/plugins/precognition.lua
@@ -0,0 +1,10 @@
+return {
+	"tris203/precognition.nvim",
+	cmd = "Precognition",
+	config = {
+		startVisible = false,
+	},
+	keys = {
+		{ "<leader>k", "<cmd>Precognition peek<cr>", desc = "peek motions" },
+	},
+}
