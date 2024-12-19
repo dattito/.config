0c6cd06ba8bc216e901cc929b7b11df671570ad1
I568850 <david.siregar@sap.com>
2024-12-19 08:22:11 +0100

    feat: enhance startup time

--- .config/nvim/lua/essi_datti/plugins/fidget.lua
+++ .config/nvim/lua/essi_datti/plugins/fidget.lua
@@ -1,17 +1,13 @@
 -- To display lsp logs in the botom right corner of the screen (mostly for rust-analyzer)
 return {
-  "j-hui/fidget.nvim",
-  lazy = false,
-  opts = {
-
-    notification = {
-
-      window = {
-        max_height = 3,
-        winblend = 0,
-      },
-    },
-
-    -- options
-  },
+	"j-hui/fidget.nvim",
+	event = "VeryLazy",
+	opts = {
+		notification = {
+			window = {
+				max_height = 3,
+				winblend = 0,
+			},
+		},
+	},
 }
