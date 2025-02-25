ba8b1d9b8254d886a18cc3e0cbb4e539b92455ce
I568850 <david.siregar@sap.com>
2025-02-25 13:43:29 +0100

    fix: smart_open crashes telescope

--- .config/nvim/lua/essi_datti/plugins/smart_open.lua
+++ .config/nvim/lua/essi_datti/plugins/smart_open.lua
@@ -1,24 +1,33 @@
 return {
-	"danielfalk/smart-open.nvim",
-	branch = "0.3.x",
-  cmd = "Telescope",
-	dependencies = {
-		"kkharji/sqlite.lua",
-		"nvim-telescope/telescope-fzy-native.nvim",
-    "nvim-telescope/telescope.nvim",
-	},
-	config = function()
-		require("telescope").load_extension("smart_open")
-	end,
-	keys = {
-		{
-			"<leader><leader>",
-			function()
-				require("telescope").extensions.smart_open.smart_open({
-					match_algorithm = "fzy",
-				})
-			end,
-			desc = "Find files",
-		},
-	},
+  "danielfalk/smart-open.nvim",
+  branch = "0.3.x",
+  dependencies = {
+    "kkharji/sqlite.lua",
+    "nvim-telescope/telescope-fzy-native.nvim",
+    -- "nvim-telescope/telescope.nvim",
+  },
+  config = function()
+    local telescope = require("telescope")
+    telescope.load_extension("smart_open")
+    telescope.setup({
+      extensions = {
+        smart_open = {
+          match_algorithm = "fzy",
+        },
+      },
+    })
+    local plugin = require("lazy.core.config").plugins["telescope.nvim"]
+    require("lazy.core.loader").reload(plugin)
+  end,
+  keys = {
+    {
+      "<leader><leader>",
+      function()
+        require("telescope").extensions.smart_open.smart_open({
+          match_algorithm = "fzy",
+        })
+      end,
+      desc = "Find files",
+    },
+  },
 }
