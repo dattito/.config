96ae147d42edf91ca4bb3c8a411428c34ca1be34
I568850 <david.siregar@sap.com>
2025-02-25 13:43:46 +0100

    fix: typstpreview uses main.tpy instead of current file

--- .config/nvim/lua/essi_datti/plugins/typst_preview.lua
+++ .config/nvim/lua/essi_datti/plugins/typst_preview.lua
@@ -2,12 +2,32 @@ return {
   "chomosuke/typst-preview.nvim",
   ft = "typst",
   version = "1.*",
-  opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+  opts = {
+    get_main_file = function(path_of_buffer)
+      local buffer_dir = vim.fn.fnamemodify(path_of_buffer, ":p:h")
+      local current_dir = buffer_dir
+      local drive = vim.fn.fnamemodify(current_dir, ":p:h:h") == "" and vim.fn.fnamemodify(current_dir, ":p:h")
+
+      -- Search up the directory hierarchy for main.typ
+      while current_dir ~= drive do
+        local main_file = current_dir .. "/main.typ"
+        if vim.fn.filereadable(main_file) == 1 then
+          return main_file
+        end
+        -- Move up to parent directory
+        current_dir = vim.fn.fnamemodify(current_dir, ":h")
+      end
+
+      -- If we reach here, no main.typ was found in parent directories
+      return path_of_buffer
+    end,
+  }, -- lazy.nvim will implicitly calls `setup {}`
   keys = {
     {
-      "<leader>tp",
+      "<leader>tt",
       "<cmd>TypstPreviewToggle<cr>",
       desc = "Typst Preview Toggle",
     },
   },
+  cmd = "TypstPreviewToggle",
 }
