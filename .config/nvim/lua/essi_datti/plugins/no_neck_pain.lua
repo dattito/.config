af5863c5145880fe90d8264bd7c2934751c1896e
I568850 <david.siregar@sap.com>
2025-03-17 09:01:48 +0100

    feat: add no-neck-pain.nvim

---
+++ .config/nvim/lua/essi_datti/plugins/no_neck_pain.lua
@@ -0,0 +1,14 @@
+return {
+  "shortcuts/no-neck-pain.nvim",
+  version = "*",
+  event = { "BufNewFile", "BufRead" },
+  opts = {
+    width = 120,
+    mappings = {
+      enabled = true,
+    },
+    autocmds = {
+      enableOnVimEnter = true,
+    },
+  },
+}
