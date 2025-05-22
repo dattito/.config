1152e6ff4d4e7a803cf26e8e53872e5c79844816
I568850 <david.siregar@sap.com>
2025-05-22 12:10:58 +0200

    feat: shell optimization

--- .config/zsh/antidote.sh
+++ .config/zsh/antidote.sh
@@ -2,7 +2,7 @@
 
 zstyle ':antidote:bundle' file "$HOME/.config/antidote/plugins.txt"
 zstyle ':antidote:static' file "$HOME/.config/antidote/plugins.zsh"
-source "$(brew --prefix)/opt/antidote/share/antidote/antidote.zsh"
+source "/opt/homebrew/opt/antidote/share/antidote/antidote.zsh"
 antidote load
 
 bindkey '^ ' autosuggest-accept
