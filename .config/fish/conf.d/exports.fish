a1a55ee2a2e134bca630da800fb111f9fe287cab
dattito <32016658+dattito@users.noreply.github.com>
2025-07-27 21:00:33 +0200

    feat(fish): use bat in man

--- .config/fish/conf.d/exports.fish
+++ .config/fish/conf.d/exports.fish
@@ -6,3 +6,5 @@ set -gx EDITOR nvim
 set -gx K9S_CONFIG_DIR ~/.config/k9s
 
 set -gx BAT_THEME "Catppuccin Mocha"
+
+set -x MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
