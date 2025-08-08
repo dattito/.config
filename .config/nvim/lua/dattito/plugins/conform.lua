a982ffb767fc4bf39fc87fe0d4da6bcc0c88bc59
dattito <32016658+dattito@users.noreply.github.com>
2025-08-08 09:52:03 +0700

    feat(nvim): add telescope-undo and add typst formatter

--- .config/nvim/lua/dattito/plugins/conform.lua
+++ .config/nvim/lua/dattito/plugins/conform.lua
@@ -17,7 +17,8 @@ return {
       rust = { "rustfmt", lsp_format = "fallback" },
       javascript = { "prettierd", "prettier", stop_after_first = true },
       go = { "goimports-reviser" },
-      typescriptreact = {"prettierd", "prettier", stop_after_first = true }
+      typescriptreact = {"prettierd", "prettier", stop_after_first = true },
+      typst = {"prettypst"}
     },
   },
 }
--- .config/nvim/lua/dattito/plugins/telescope_fzy_native.lua
+++ .config/nvim/lua/dattito/plugins/telescope_fzy_native.lua
@@ -1,8 +1,16 @@
 return {
-  "nvim-telescope/telescope-fzy-native.nvim",
-  dependencies = "nvim-telescope/telescope.nvim",
-  cmd = "Telescope",
-  config = function()
-    require("telescope").load_extension("fzy_native")
-  end,
+	"nvim-telescope/telescope-fzy-native.nvim",
+	dependencies = "nvim-telescope/telescope.nvim",
+	cmd = "Telescope",
+	config = function()
+		require("telescope").setup({
+			extensions = {
+				fzy_native = {
+					override_generic_sorter = true,
+					override_file_sorter = true,
+				},
+			},
+		})
+		require("telescope").load_extension("fzy_native")
+	end,
 }
