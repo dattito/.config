ef339245c83b072041a9152e54986c7e465102be
dattito <32016658+dattito@users.noreply.github.com>
2025-09-01 22:10:35 +0200

    feat: add zellij-nvim

--- .config/nvim/lua/dattito/plugins/zellij_nav.lua
+++ .config/nvim/lua/dattito/plugins/zellij_nav.lua
@@ -1,16 +1,32 @@
 return {
 	"https://git.sr.ht/~swaits/zellij-nav.nvim",
-	cmd = {
-		"ZellijNavigateLeft",
-		"ZellijNavigateDown",
-		"ZellijNavigateUp",
-		"ZellijNavigateRight",
-	},
 	keys = {
-		{ "<c-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
-		{ "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
-		{ "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
-		{ "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
+		{
+			"<c-h>",
+			"<cmd>ZellijNavigateLeft<cr>",
+			{ silent = true, desc = "navigate left" },
+			mode = { "i", "n", "v" },
+		},
+		{
+			"<c-j>",
+			"<cmd>ZellijNavigateDown<cr>",
+			{ silent = true, desc = "navigate down" },
+			mode = { "i", "n", "v" },
+		},
+
+		{
+			"<c-k>",
+			"<cmd>ZellijNavigateUp<cr>",
+			{ silent = true, desc = "navigate up" },
+			mode = { "i", "n", "v" },
+		},
+		{
+			"<c-l>",
+			"<cmd>ZellijNavigateRight<cr>",
+			{ silent = true, desc = "navigate right" },
+
+			mode = { "i", "n", "v" },
+		},
 	},
 	opts = {},
 }
